400bdec847974f25eea0c94794670ae60a231069
amingilani <amin@gilani.me>
2016-12-12 14:28:01 +0500

    Echo instead of print

--- initialize.sh
+++ initialize.sh
@@ -10,13 +10,13 @@ echo "*.*          @$2.papertrailapp.com:$3'" >> /etc/rsyslog.conf
 sudo service rsyslog restart
 
 # regular maintenance
-print "performing upgrades"
+echo "performing upgrades"
 sudo apt-get update && sudo apt-get upgrade -y
 
 # go home
 cd
 
 # execute the real script
-print "running worker"
+echo "running worker"
 git clone https://gist.github.com/3a2c35a0e12dea88d48b6d8e6a621cee.git scripts
 tmux new-session -d "cat scripts/miner.sh | bash -s $1"
