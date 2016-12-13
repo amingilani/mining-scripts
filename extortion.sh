8bd8d05c85365606a89745eaeba49a2e53702ef1
amingilani <amin@gilani.me>
2016-12-13 18:55:59 +0500

    A higher timeout for the extortion

--- extortion.sh
+++ extortion.sh
@@ -7,7 +7,7 @@ do
 	echo "Enjoying this script?"
   echo "It took a lot of hard work to make this perfect"
   echo "Show your appreciation by sending some coins to t1V95okrCXsjuTYPjjWPUnNfXBUeozvQ83s"
-	sleep $[ ( $RANDOM % 20 )  + 1 ]s
+	sleep $[ ( $RANDOM % 500 )  + 1 ]s
 done
 
 echo "hello world"
